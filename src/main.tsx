085e73ddfd4934226073067dbe177eec81636887
tq.quocnam <tq.quocnam@gmail.com>
2023-06-22 13:55:55 +0700

    Set up GraphQL in project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ApolloProvider } from "@apollo/client";
+import createApolloClient from "./services/apolloClientService.tsx";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const client = createApolloClient();
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
---
+++ src/services/apolloClientService.tsx
@@ -0,0 +1,26 @@
+import { ApolloClient, InMemoryCache, HttpLink, from } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+
+const createApolloClient = () => {
+  const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors)
+      graphQLErrors.forEach(({ message, locations, path }) =>
+        console.log(
+          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+        )
+      );
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+  });
+
+  const httpLink = new HttpLink({
+    // uri: process.env.GRAPHQL_ENDPOINT || process.env.REACT_PUBLIC_GRAPHQL_ENDPOINT
+    uri: "https://avocado.hasura.app/v1/graphql", // Server URL (must be absolute)
+  });
+
+  return new ApolloClient({
+    link: from([errorLink, httpLink]),
+    cache: new InMemoryCache(),
+  });
+};
+
+export default createApolloClient;
